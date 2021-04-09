dd23eacf57f4d8a185dc2a4a220a92adc2e2e67f
Santiago Beltran <santiago.beltran@marsroboticsassociation.org>
2021-04-09 11:01:54 -0400

    Returned to old XHTTPRequests

--- Messager.js
+++ Messager.js
@@ -2,58 +2,43 @@ var messages = document.getElementById("messages");
 
 var msgInput = document.getElementById("msgInput");
 
-var url = "https://api.isaacthoman.me/api/App";
+var url = "https://api.beanchat.isaacthoman.me/api/App";
 
-function Messages(msg){
-  if(!!msg)messages.innerHTML = msg;
-  return messages.innerHTML;
+function Messages(msg) {
+	if (!!msg) messages.innerHTML = msg;
+	return messages.innerHTML;
 }
 
 async function Send() {
-  var msg = msgInput.value;
-  if (msg === "") return;
-  msgInput.value = "";
+	var msg = msgInput.value;
+	if (msg === "") return;
+	msgInput.value = "";
 
-  try {
-    var response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "text/html;charset=utf-8"
-      },
-      message: msg
-    });
-  } catch (e) {
-    this.error = e;
-    return;
-  }
-
-  if (response.ok) {
-    console.log(await response.text());
-    Refresh();
-  } else {
-    Messages(`HTTP-Error: ${response.status}`);
-  }
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.readyState === 4 && this.status === 200) {
+			Refresh();
+		}
+	};
+	xhttp.open("POST", `${url}?message=${msg}`, true);
+	xhttp.send();
 }
 
 document.getElementById("send").onclick = function () {
-  Send();
+	Send();
 };
 
 async function Refresh() {
-  try {
-    var response = await fetch(url);
-  } catch (e) {
-
-    messages.innerHTML = `HTTP-Error: ${e}`;
-    return;
-  }
-  if (response.ok) {
-    Messages(`HTTP-Error: ${await response.text()}`);
-  } else {
-    Messages(`HTTP-Error: ${response.status}`);
-  }
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.readyState === 4 && this.status === 200) {
+			Messages(this.responseText);
+		}
+	};
+	xhttp.open("GET", "url", true);
+	xhttp.send();
 }
 
 document.getElementById("refresh").onclick = function () {
-  Refresh();
+	Refresh();
 };
