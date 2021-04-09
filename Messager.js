06d4c16c514d7a7a89d6d942e0ad5076de12d04b
Santiago Beltran <santiago.beltran@marsroboticsassociation.org>
2021-04-09 11:17:05 -0400

    Error Handling

--- Messager.js
+++ Messager.js
@@ -2,7 +2,7 @@ var messages = document.getElementById("messages");
 
 var msgInput = document.getElementById("msgInput");
 
-var url = "https://api.beanchat.isaacthoman.me/api/App";
+var url = "https://api.isaacthoman.me/api/App";
 
 function Messages(msg) {
 	if (!!msg) messages.innerHTML = msg;
@@ -13,7 +13,7 @@ async function Send() {
 	var msg = msgInput.value;
 	if (msg === "") return;
 	msgInput.value = "";
-
+	try{
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function () {
 		if (this.readyState === 4 && this.status === 200) {
@@ -22,6 +22,7 @@ async function Send() {
 	};
 	xhttp.open("POST", `${url}?message=${msg}`, true);
 	xhttp.send();
+}catch(e){Messages(`HTTPS-ERROR ${e}`);}
 }
 
 document.getElementById("send").onclick = function () {
@@ -29,6 +30,7 @@ document.getElementById("send").onclick = function () {
 };
 
 async function Refresh() {
+	try{
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function () {
 		if (this.readyState === 4 && this.status === 200) {
@@ -37,6 +39,7 @@ async function Refresh() {
 	};
 	xhttp.open("GET", "url", true);
 	xhttp.send();
+}catch(e){Messages(`HTTPS-ERROR ${e}`);}
 }
 
 document.getElementById("refresh").onclick = function () {
